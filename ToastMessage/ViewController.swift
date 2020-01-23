4902665707dd08687cd2b0b2b1e78ad78b428b3f
Ievgenii Poberezhnyi <ievgenii_poberezhnyi@epam.com>
2020-01-23 17:14:39 +0200

    Initial Commit

---
+++ ToastMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToastMessage
+//
+//  Created by Ievgenii Poberezhnyi on 23.01.2020.
+//  Copyright © 2020 Ievgenii Poberezhnyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
